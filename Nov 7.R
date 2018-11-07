70f4d65f83bb82fe5263a117fd2c527a63b59b7e
LeticiaBaSm <lbs97@cornell.edu>
2018-11-07 14:58:16 -0500

    Added files to repo

---
+++ Nov 7.R
@@ -0,0 +1,12 @@
+## Github - Nov 6 Version Control
+
+#Github name: LeticiaBaSm
+# pass = 1455...
+
+#Had to go into Terminal to link Git in the computer with Github website
+#
+
+#created new repository
+
+#Create new "version control" project as Git
+#gitignore = things you dont want to go to git
